e0ecc71785a2bfc09c63218b16ff274d917ab530
Sara Jensen <sara.jensen@idealist.org>
2016-09-05 19:03:40 -0700

    start Animal and Vehicle class projects

---
+++ Animals.java
@@ -0,0 +1,7 @@
+class Animals {
+  public int mAge;
+  public String mSpecies;
+  public String mColor;
+  public String mFavoriteTreat;
+  public int mTimeInShelter;
+}
---
+++ Vehicle.java
@@ -0,0 +1,7 @@
+class Vehicle {
+  public int mYear;
+  public String mBrand;
+  public String mModel;
+  public int mMiles;
+  public int mPrice;
+}
