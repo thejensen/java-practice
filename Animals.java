33e324308bd5d18a31486b30065c31680268aa9e
Sara Jensen <sara.jensen@idealist.org>
2016-09-05 20:51:46 -0700

    save files from the roadtrip.

--- Animals.java
+++
@@ -1,7 +0,0 @@
-class Animals {
-  public int mAge;
-  public String mSpecies;
-  public String mColor;
-  public String mFavoriteTreat;
-  public int mTimeInShelter;
-}
--- Vehicle.java
+++
@@ -1,7 +0,0 @@
-class Vehicle {
-  public int mYear;
-  public String mBrand;
-  public String mModel;
-  public int mMiles;
-  public int mPrice;
-}
